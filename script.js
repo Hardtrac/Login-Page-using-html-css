b7373749947c7a50819ff2c68b8ecd45daa35c1f
Hardtrac <162275055+Hardtrac@users.noreply.github.com>
2024-06-05 16:02:19 +0530

    script.js

--- script.js
+++ script.js
@@ -1,48 +1,61 @@
-const form = document.querySelector("form");
-eField = form.querySelector(".email"),
-    eInput = eField.querySelector("input"),
-    pField = form.querySelector(".password"),
-    pInput = pField.querySelector("input");
+<form action="https://docs.google.com/spreadsheets/d/14KXopA1e_moj2898_TaeyV0G0lbau87RK68AjM28rJ4/edit#gid=0" method="POST">
+    <div class="email">
+        <input type="text" placeholder="Email" required>
+        <span class="error-txt">Email can't be blank</span>
+    </div>
+    <div class="password">
+        <input type="password" placeholder="Password" required>
+        <span class="error-txt">Password can't be blank</span>
+    </div>
+    <button type="submit">Login</button>
+</form>
 
-form.onsubmit = (e) => {
-    e.preventDefault();
+<script>
+    const form = document.querySelector("form");
+    const eField = form.querySelector(".email"),
+        eInput = eField.querySelector("input"),
+        pField = form.querySelector(".password"),
+        pInput = pField.querySelector("input");
 
-    (eInput.value == "") ? eField.classList.add("shake", "error"): checkEmail();
-    (pInput.value == "") ? pField.classList.add("shake", "error"): checkPass();
+    form.onsubmit = (e) => {
+        e.preventDefault();
 
-    setTimeout(() => {
-        eField.classList.remove("shake");
-        pField.classList.remove("shake");
-    }, 500);
+        (eInput.value == "") ? eField.classList.add("shake", "error") : checkEmail();
+        (pInput.value == "") ? pField.classList.add("shake", "error") : checkPass();
 
-    eInput.onkeyup = () => { checkEmail(); }
-    pInput.onkeyup = () => { checkPass(); }
+        setTimeout(() => {
+            eField.classList.remove("shake");
+            pField.classList.remove("shake");
+        }, 500);
 
-    function checkEmail() {
-        let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-        if (!eInput.value.match(pattern)) {
-            eField.classList.add("error");
-            eField.classList.remove("valid");
-            let errorTxt = eField.querySelector(".error-txt");
+        eInput.onkeyup = () => { checkEmail(); }
+        pInput.onkeyup = () => { checkPass(); }
 
-            (eInput.value != "") ? errorTxt.innerText = "Enter a valid email address": errorTxt.innerText = "Email can't be blank";
-        } else {
-            eField.classList.remove("error");
-            eField.classList.add("valid");
+        function checkEmail() {
+            const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+            const errorTxt = eField.querySelector(".error-txt");
+            if (!eInput.value.match(pattern)) {
+                eField.classList.add("error");
+                eField.classList.remove("valid");
+                eInput.value != "" ? errorTxt.innerText = "Enter a valid email address" : errorTxt.innerText = "Email can't be blank";
+            } else {
+                eField.classList.remove("error");
+                eField.classList.add("valid");
+            }
+        }
+
+        function checkPass() {
+            if (pInput.value == "") {
+                pField.classList.add("error");
+                pField.classList.remove("valid");
+            } else {
+                pField.classList.remove("error");
+                pField.classList.add("valid");
+            }
+        }
+
+        if (!eField.classList.contains("error") && !pField.classList.contains("error")) {
+            window.location.href = form.getAttribute("action");
         }
     }
-
-    function checkPass() {
-        if (pInput.value == "") {
-            pField.classList.add("error");
-            pField.classList.remove("valid");
-        } else {
-            pField.classList.remove("error");
-            pField.classList.add("valid");
-        }
-    }
-
-    if (!eField.classList.contains("error") && !pField.classList.contains("error")) {
-        window.location.href = form.getAttribute("action");
-    }
-}
+</script>
